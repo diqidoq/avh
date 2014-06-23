6d2061ea7a2c5707de1718d1272858e0bc2a8d29
diqidoq <support@digidog.de>
2014-06-24 01:29:50 +0200

    diqidoq: install.sh (bin mkdir/ permission handling) added.

--- install.sh
+++ install.sh
@@ -3,7 +3,20 @@
 # 2012 (c) diqidoq, for MAROQQO digital media
 #
 
-sudo cp avh.sh ~/bin/avh
-sudo chmod -R 776 ~/bin ~/bin/avh
-sudo chmod a+x ~/bin/avh
+tD=~/bin2
 
+installAVH(){
+  sudo cp avh.sh "${tD}"/avh
+  sudo chmod -R 776 "${tD}" "${tD}"/avh
+  sudo chmod a+x "${tD}"/avh
+  echo "${tD}/avh insalled and permission set executable (chmod a+x)."
+}
+
+if [ -d "${tD}" ]; then
+  installAVH
+else
+  mkdir "${tD}"
+  chmod 775 "${tD}"
+  echo "${tD} created"
+  installAVH
+fi
